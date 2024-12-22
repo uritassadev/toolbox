a9a490a0659730ed19bb88d9b4495d336f5e8164
Uritassa <uri.t@logicalcommander.com>
2024-12-22 18:52:16 +0200

    removed tf configurations

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "" // TODO: replace with your bucket
-    key    = "terraform/terraform.tfstate"
-    region = "" // TODO: replace with your region
-    encrypt = true
-  }
-}
